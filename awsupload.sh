95f7762a2bfad32ea44b99c47a51d797ad1a4958
Mike Furby <49192760+mikefurby@users.noreply.github.com>
2020-06-25 16:41:06 +0100

    Create awsupload.sh
    this uploads the league html to s3 buckets which are used to host the main website

---
+++ awsupload.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+club=$1
+
+#UPLOAD
+awsdst="s3://vscc-challenges.cc/$club/"
+aws s3 cp ~/$club/html/index.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/html/index.html s3://vscc-challenges.cc/$club/index.htm --region eu-west-1
+aws s3 cp ~/$club/html/current.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/html/seasontable.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/html/liveleague1.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/html/liveleague2.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/html/liveleague3.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/html/liveleague4.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/html/liveleague5.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/html/liveleague6.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/html/liveleague7.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/html/liveleague8.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/html/liveleague9.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/html/liveleague10.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/html/liveleague11.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/html/matrix11.html $awsdst --region eu-west-1
+aws s3 cp ~/$club/get$club.sh $awsdst --region eu-west-1
+aws s3 cp ~/$club/archive/seasontable20201.html $awsdst --region eu-west-1
