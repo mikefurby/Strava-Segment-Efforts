8da0d67e5774009f35929127708a7fc559d54e5c
Mike Furby <49192760+mikefurby@users.noreply.github.com>
2020-06-25 16:44:16 +0100

    Create concatsegments.sh
    cats all the different club segment lists into one because I have one shared APP that all activities are reported via web hook.

---
+++ concatsegments.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cat /home/ubuntu/ycf/input/segmentsummary.csv > /home/ubuntu/vscc/input/segmentsummary.csv
+cat /home/ubuntu/vscctt/input/segmentsummary.csv >> /home/ubuntu/vscc/input/segmentsummary.csv
+cat /home/ubuntu/vstc/input/segmentsummary.csv >> /home/ubuntu/vscc/input/segmentsummary.csv
+cat /home/ubuntu/vs3l/input/segmentsummary.csv >> /home/ubuntu/vscc/input/segmentsummary.csv
+cat /home/ubuntu/vsvl/input/segmentsummary.csv >> /home/ubuntu/vscc/input/segmentsummary.csv
+cat /home/ubuntu/grimpeur/input/segmentsummary.csv >> /home/ubuntu/vscc/input/segmentsummary.csv
